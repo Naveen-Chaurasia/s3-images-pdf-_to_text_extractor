e2705741116ba5d2e61c80ea56fad8fa34fab164
Naveen <naveen.chaurasia861@gmail.com>
2023-01-30 14:00:30 +0530

    added pdf tables extractor.py

--- app1.py
+++ app1.py
@@ -1,12 +1,7 @@
-#for extracting contents in json format
-
 from flask import Flask, jsonify
 import boto3
 import time
 app = Flask(__name__)
-
-
-
 def startJob(s3BucketName, objectName):
     response = None
     client = boto3.client('textract',region_name='us-east-1')
@@ -17,30 +12,22 @@ def startJob(s3BucketName, objectName):
             'Name': objectName
         }
     })
-
     return response["JobId"]
-
 def isJobComplete(jobId):
     time.sleep(5)
     client = boto3.client('textract',region_name='us-east-1')
     response = client.get_document_text_detection(JobId=jobId)
     status = response["JobStatus"]
     print("Job status: {}".format(status))
-
     while(status == "IN_PROGRESS"):
         time.sleep(5)
         response = client.get_document_text_detection(JobId=jobId)
         status = response["JobStatus"]
         print("Job status: {}".format(status))
-
     return status
-
 def getJobResults(jobId):
-
     pages = []
-
     time.sleep(5)
-
     client = boto3.client('textract',region_name='us-east-1')
     response = client.get_document_text_detection(JobId=jobId)
     
@@ -49,36 +36,27 @@ def getJobResults(jobId):
     nextToken = None
     if('NextToken' in response):
         nextToken = response['NextToken']
-
     while(nextToken):
         time.sleep(5)
-
         response = client.get_document_text_detection(JobId=jobId, NextToken=nextToken)
-
         pages.append(response)
         print("Resultset page recieved: {}".format(len(pages)))
         nextToken = None
         if('NextToken' in response):
             nextToken = response['NextToken']
-
     return pages
-
 # Document
 s3BucketName = "naveen-chaurasia-bucket"
-documentName = ""
-
-
+documentName = "1645ed46_cd9c_496e_a1c3_4e637fb0c541_w461c132_r1_3_inch_camlock_pipe.pdf"
 @app.route("/")
 def home():
-
-
     jobId = startJob(s3BucketName, documentName)
     print("Started job with id: {}".format(jobId))
     if(isJobComplete(jobId)):
         response = getJobResults(jobId)
-
     #print(response)
-    return response
+    # return response
+    return jsonify({"data":response})
     # # Print detected text
     # for resultPage in response:
     #     for item in resultPage["Blocks"]:
