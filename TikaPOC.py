79a85395f9f47a0c2e9f8635c75a8e0f3886de66
Naveen <naveen.chaurasia861@gmail.com>
2023-02-12 10:32:18 +0530

    added Tikapoc.py

---
+++ TikaPOC.py
@@ -0,0 +1,18 @@
+#https://www.tutorialspoint.com/tika/tika_overview.htm
+#Apache Mahout/UIMA
+# import parser object from tike
+from tika import parser  
+  
+# opening pdf file
+parsed_pdf = parser.from_file("sample.pdf")
+  
+# saving content of pdf
+# you can also bring text only, by parsed_pdf['text'] 
+# parsed_pdf['content'] returns string 
+data = parsed_pdf['content'] 
+  
+# Printing of content 
+print(data)
+  
+# <class 'str'>
+print(type(data))
